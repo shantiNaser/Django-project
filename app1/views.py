017419a9a26cb9f90f88a0a8fc69310dc34c1d4c
NaserShanti <nasershanti654@gmail.com>
2021-05-19 14:19:50 +0300

    add template folder , and create an html web page inside it

--- app1/views.py
+++ app1/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, World .. Test Deployment  ")
+    # return HttpResponse("Hello, World .. Test Deployment  ")
+    return render(request, "home.html")
